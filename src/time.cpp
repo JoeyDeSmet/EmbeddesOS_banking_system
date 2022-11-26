b686694e64a99aca34cdd349e107b38a371e0beb
Joey <joey@de-smet.vlaanderen>
2022-11-26 13:33:04 +0100

    Maby fixed issue

--- src/time.cpp
+++ src/time.cpp
@@ -12,13 +12,13 @@ namespace Banking {
     auto s_this = (Time *) arg;
 
     while (true) {
-      s_this->m_midnight_mtx.lock();
       // Add time
       s_this->m_time_mtx.trylock_for(rtos::Kernel::wait_for_u32_forever);
       s_this->m_time = (s_this->m_time + SECONDS_PER_SECOND) % SECONDS_IN_DAY;
       s_this->m_time_mtx.unlock();
 
       if (s_this->m_time > TIME_20_HOUR || s_this->m_time < TIME_7_HOUR) {
+        s_this->m_midnight_mtx.lock();
         s_this->m_is_midnight.notify_all();
         s_this->m_midnight_mtx.unlock();
       }
